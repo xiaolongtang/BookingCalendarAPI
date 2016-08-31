23f742ae053570636a0b003de7995fcc34e9401f
Tang <xiaolong.tang@pactera.com>
2016-08-31 14:38:18 +0800

    role interface done

--- src/com/pactera/servlet/GetRoleServlet.java
+++ src/com/pactera/servlet/GetRoleServlet.java
@@ -20,16 +20,19 @@ public class GetRoleServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberID=request.getParameter("member");
 		RoleUtil ru=new RoleUtil();
-//		System.out.println(ru.checkGroupMembership("feea4630-59c5-4eb4-b263-a182ab5097ed","5abf9656-604d-4d80-ad4f-04b1a4eed78d"));
-//		System.out.println(ru.checkGroups("5abf9656-604d-4d80-ad4f-04b1a4eed78d"));
-		String role=ru.checkGroups("5abf9656-604d-4d80-ad4f-04b1a4eed78d");
+//		String role=ru.checkGroups("5abf9656-604d-4d80-ad4f-04b1a4eed78d");
+		String role=ru.checkGroups(memberID);
 		String json="{\"role\":\""+role+"\"}";
 		returnWriter(response,json);
 	}
 
     private void returnWriter(HttpServletResponse response, String json) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");  
+        response.setHeader("Access-Control-Allow-Methods","*");  
+        response.setHeader("Access-Control-Allow-Headers","x-requested-with,content-type"); 
         PrintWriter out = response.getWriter();
         out.println(json);
     }
